57306f626ee92dacfabec614fc21bd2452a6ff9c
MykhCher <micher1240@gmail.com>
2024-10-01 13:55:34 +0300

    refactor LoginForm into functional components

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Field, Form, Formik } from 'formik';
+import { Form, Formik } from 'formik';
+// =====
 import { checkAuth, clearAuth } from '../../store/slices/authSlice';
-import styles from './LoginForm.module.sass';
-import FormInput from '../FormInput/FormInput';
 import Schems from '../../utils/validators/validationSchems';
-import Error from '../Error/Error';
+import FormInput from '../FormInput/FormInput';
+import styles from './LoginForm.module.sass';
 import CONSTANTS from '../../constants';
+import Error from '../Error/Error';
 
-class LoginForm extends React.Component {
-  componentWillUnmount() {
-    this.props.authClear();
-  }
+function LoginForm(props) {
 
-  clicked = (values) => {
-    this.props.loginRequest({ data: values, navigate: this.props.navigate });
+  useEffect(() => {
+    return () => {
+      props.authClear();
+    }
+  }, []);
+
+  const clicked = (values) => {
+    props.loginRequest({ data: values, navigate: props.navigate });
   };
 
-  render() {
-    const { error, isFetching } = this.props.auth;
-    const { submitting, authClear } = this.props;
+    const { error, isFetching } = props.auth;
+    const { submitting, authClear } = props;
 
     const formInputClasses = {
       container: styles.inputContainer,
@@ -44,7 +47,7 @@ class LoginForm extends React.Component {
             email: '',
             password: '',
           }}
-          onSubmit={this.clicked}
+          onSubmit={clicked}
           validationSchema={Schems.LoginSchem}
         >
           <Form>
@@ -73,7 +76,6 @@ class LoginForm extends React.Component {
         </Formik>
       </div>
     );
-  }
 }
 
 const mapStateToProps = (state) => {
