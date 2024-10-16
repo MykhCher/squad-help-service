820142df5134156a187f8abc1ee2df5cc806af89
MykhCher <micher1240@gmail.com>
2024-10-16 19:36:16 +0300

    add style for domain button group

--- client/src/components/ButtonGroup/ButtonGroup.jsx
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -1,5 +1,11 @@
 import { Field } from 'formik';
 import { useLayoutEffect } from 'react'
+// =====
+import styles from './ButtonGroup.module.sass';
+
+const radioStyle = {position: 'absolute', opacity: 0, pointerEvents: null};
+const headerStyle = (value, field) => ({color: field === value ? 'inherit' : '#0009'});
+const borderStyle = (value, field) => ({borderColor: field === value ? 'blue' : '#ccc'});
 
 function ButtonGroup({ name, values: { domain }, ...rest }) {
 
@@ -10,26 +16,28 @@ function ButtonGroup({ name, values: { domain }, ...rest }) {
   })
 
   return (
-    <div>
-        <label id="domain-choice">Do you want a matching domain (.com URL) with your name?</label>
-        <div>
-            <label>
-            <Field type="radio" name={name} value="allowMinorVariations"/>
-            Yes (But minor variations are allowed)
-            </label>
-        </div>
-        <div>
-            <label>
-            <Field type="radio" name={name} value="exactMatch" />
-            Yes (The domain should exactly match the name)
-            </label>
-        </div>
-        <div>
-            <label>
-            <Field type="radio" name={name} value="noDomain" />
-            No (I am only looking for a name, not a domain)
-            </label>
-        </div>
+    <div className={styles.fieldContainer}>
+      <label id="domain-choice" className={styles.domainChoice}>Do you want a matching domain (.com URL) with your name?</label>
+      <div className={styles.optionContainer}>
+          <div>
+              <label className={styles.option}style={borderStyle("allowMinorVariations", domain)}>
+              <Field type="radio" name={name} value="allowMinorVariations" style={radioStyle}/>
+              <span className={styles.header} style={headerStyle("allowMinorVariations", domain)}>Yes</span> <span className={styles.description}>But minor variations are allowed</span>
+              </label>
+          </div>
+          <div>
+              <label className={styles.option}style={borderStyle("exactMatch", domain)}>
+              <Field type="radio" name={name} value="exactMatch" style={radioStyle} />
+              <span className={styles.header} style={headerStyle("exactMatch", domain)}>Yes</span> <span className={styles.description}>The domain should exactly match the name</span>
+              </label>
+          </div>
+          <div>
+              <label className={styles.option}style={borderStyle("noDomain", domain)}>
+              <Field type="radio" name={name} value="noDomain" style={radioStyle}/>
+              <span className={styles.header} style={headerStyle("noDomain", domain)}>No</span> <span className={styles.description}>I am only looking for a name, not a domain</span>
+              </label>
+          </div>
+      </div>
     </div>
   )
 }
