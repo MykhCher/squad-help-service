c0e9e84febe660a41b4cf9ea3bf094465c8935d4
MykhCher <micher1240@gmail.com>
2024-11-10 17:23:11 +0200

    moderator auth

--- admin/app.js
+++ admin/app.js
@@ -1,10 +1,13 @@
-import AdminJS from 'adminjs';
+import AdminJS, { DefaultAuthProvider } from 'adminjs';
 import AdminJSExpress from '@adminjs/express';
 import express from 'express';
 import { Resource, Database } from '@adminjs/sql';
 // =====
+import authenticate from './auth.js';
 import options from './config/adminConfig.js';
-import { ADMIN_PORT, HOST } from './constants/index.js';
+import { ADMIN_PORT, HOST, AUTH_SECRET } from './constants/index.js';
+import { sessionStore } from './config/dbConfig.js';
+import { componentLoader } from './components/index.js';
 
 AdminJS.registerAdapter({
   Database,
@@ -14,12 +17,34 @@ AdminJS.registerAdapter({
 
 const start = async () => {
 
+
+  const authProvider = new DefaultAuthProvider({
+    componentLoader,
+    authenticate,
+  });
+
   const app = express();
 
   const admin = new AdminJS(await options());
   admin.watch();
 
-  const adminRouter = AdminJSExpress.buildRouter(admin)
+  // const adminRouter = AdminJSExpress.buildRouter(admin)
+
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin, 
+    {
+      provider: authProvider,
+      cookieName: 'adminjs',
+      cookiePassword: AUTH_SECRET,
+    },
+    null,
+    {
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: true,
+      secret: AUTH_SECRET,
+    }
+  )
 
   app.use(admin.options.rootPath, adminRouter);
 
--- admin/auth.js
+++ admin/auth.js
@@ -1,20 +1,20 @@
-// import bcrypt from 'bcrypt';
-// // =====
-// import dbCon from './config/dbConfig.js';
+import bcrypt from 'bcrypt';
+// =====
+import dbCon from './config/dbConfig.js';
 
-// const authenticate = async ({ email, password }, ctx) => {
-//   const db = await dbCon;
-//   const [ user ] = await db.table('Users').knex.select().from('Users').where({ email });
+const authenticate = async ({ email, password }, ctx) => {
+  const db = await dbCon;
+  const [ user ] = await db.table('Users').knex.select().from('Users').where({ email });
 
-//   if (
-//     user 
-//     && bcrypt.compareSync(password, user.password) 
-//     && user.role === 'moderator'
-//   ) {
-//     return Promise.resolve(user);
-//   }
+  if (
+    user
+    && bcrypt.compareSync(password, user.password)
+    && user.role === 'moderator'
+  ) {
+    return Promise.resolve(user);
+  }
 
-//   return null;
-// }
+  return null;
+}
 
-// export default authenticate;
+export default authenticate;
